f7b34bcc9150b971cc124ec5c4ff3639ee436536
Nikita Lebedev <lebedev@iskrauraltel.ru>
2018-11-22 15:55:39 +0500

    bring animation back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { InMemoryDataService } from './in-memory-data.service';
@@ -22,7 +22,7 @@ import { EditTaskComponent } from './edit-task/edit-task.component';
     AppRoutingModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
-    NoopAnimationsModule,
+    BrowserAnimationsModule,
     AppMaterialModule,
   ],
   providers: [],
